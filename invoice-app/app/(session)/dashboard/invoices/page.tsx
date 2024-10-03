b586881b6a62d2645456fff416337f56877d0e41
DavidBatkay <david.batkay@emanuel.ro>
2024-10-03 15:32:21 +0300

    added invoice objects on the invoice apps (tried with db but not working)

--- invoice-app/app/(session)/dashboard/invoices/page.tsx
+++ invoice-app/app/(session)/dashboard/invoices/page.tsx
@@ -1,7 +1,18 @@
-const InvoicePage: React.FC = () => {
+import { prisma } from "@/lib/prisma";
+
+const InvoicePage: React.FC = async () => {
+  const invoices = [
+    { id: 123, name: "Microsoft Invoice" },
+    { id: 124, name: "Luxoft Invoice" },
+  ]; //await prisma.post.findMany(); BUG gives error, idk
+
   return (
     <div className="flex flex-col min-h-screen justify-center items-center">
-      Invoice Page
+      <ul className="flex flex-col gap-y-2">
+        {invoices.map((invoice: any) => (
+          <li key={invoice.id}>{invoice.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
