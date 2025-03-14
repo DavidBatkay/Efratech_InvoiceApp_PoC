77929c6d3a690d79f3ca1e0cf021a7ed21e20996
David Batkay <david.batkay@emanuel.ro>
2025-03-14 12:13:32 +0200

    payment notes confirm update button - changed color

--- invoice-app/components/paymentFormEdit.tsx
+++ invoice-app/components/paymentFormEdit.tsx
@@ -76,7 +76,7 @@ const PaymentFormEdit = ({
                 Cancel
               </button>
               <button
-                className="px-4 py-2 bg-red-500 text-white rounded-md"
+                className="px-4 py-2 bg-green-500 text-white rounded-md"
                 onClick={handleSubmit}
                 disabled={isSubmitting}
               >
